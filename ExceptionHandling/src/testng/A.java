7b95ebd56b2d6b91e6cdf92d1c6b059858cac840
TestingWorld <TestingWorld@TestingWorld-PC>
2016-09-25 08:47:35 +0530

    TASK-IDdsfds

---
+++ ExceptionHandling/src/testng/A.java
@@ -0,0 +1,16 @@
+package testng;
+
+import java.io.IOError;
+
+public class A {
+
+	public static void main(String[] args) {
+	
+	
+	   int a=10;
+	   int b=0;
+	   int c=a/b;
+	   System.out.println(c);
+	
+	}
+}
